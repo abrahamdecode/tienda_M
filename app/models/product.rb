905e066e604df6ca074bddcda63697ddf4826e5b
Abraham Espinosa <decodexdc@gmail.com>
2016-11-26 00:48:52 -0600

    se agregaron validaciones faltantes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ActiveRecord::Base
   validates :name,presence:true
-  validates :price, numericality: { only_integer: true }
+  validates :price, numericality: { only_integer: true ,:less_than_or_equal_to => 100}
 end
